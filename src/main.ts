7393ef3cb02bdd9ac224afb52c581e7f356143c6
Shim Won <marocchino@gmail.com>
2017-02-12 07:27:48 +0900

    Extract api

--- src/main.ts
+++ src/main.ts
@@ -1,42 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import * as Vue from 'vue'
-import ApolloClient, { createNetworkInterface } from 'apollo-client'
 import * as Vuex from 'vuex'
 import App from './App'
-import gql from 'graphql-tag'
 
-// Create the apollo client
-const apolloClient = new ApolloClient({
-  networkInterface: createNetworkInterface({
-    uri: 'http://localhost:4000/api',
-    opts: {
-      transportBatching: true,
-    }
-  }),
-});
+import Api from './api'
 
-interface Query { allTasks: any }
-(<any>window).__APOLLO_QUERY__ = {
-  allTasks: gql`
-    query Tasks {
-      tasks(first: 5) {
-        edges {
-          cursor
-          node {
-            id
-            type
-            description
-          }
-        }
-        pageInfo {
-          hasNextPage
-        }
-      }
-    }`
-} as Query
-(<any>window).__APOLLO_CLIENT__ = apolloClient
-// window.__APOLLO_CLIENT__.query({query: window.__APOLLO_QUERY__.allTasks})
+Api.allTasks()
+
 Vue.use(Vuex)
 
 // extends interface option
