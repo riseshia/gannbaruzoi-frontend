da00e2383bcdb95b4937f1978ee2326e05ebf2cb
Shim Won <marocchino@gmail.com>
2018-04-18 18:28:31 +0900

    refactor: simplify api

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,3 @@
-import Tasks from './Tasks.graphql'
-import CreateTask from './CreateTask.graphql'
 import fetch from 'isomorphic-fetch'
 
 export async function query(graphqlQuery, variables) {
@@ -16,11 +14,17 @@ export async function query(graphqlQuery, variables) {
   const json = await response.json()
   return json
 }
-
-export async function tasks(variables) {
-  return query(Tasks, variables)
-}
-
-export async function createTask(variables) {
-  return query(CreateTask, variables)
+export async function mutate(graphqlQuery, variables) {
+  const response = await fetch('http://localhost:4000/api', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: graphqlQuery.loc.source.body,
+      variables,
+    }),
+  })
+  const json = await response.json()
+  return json
 }
--- src/store/actions.js
+++ src/store/actions.js
@@ -1,4 +1,6 @@
-import { tasks, createTask } from '@/api/index'
+import { query } from '@/api/index'
+import Tasks from '@/api/Tasks.graphql'
+import CreateTask from '@/api/CreateTask.graphql'
 
 export default {
   updateNewTaskDescription({ commit }, description) {
@@ -10,7 +12,7 @@ export default {
       return
     }
     commit('START_LOADING')
-    const payload = await tasks(variables)
+    const payload = await query(Tasks, variables)
     commit('UPDATE_TASKS', payload)
     commit('FINISH_LOADING')
   },
@@ -21,7 +23,7 @@ export default {
     }
     commit('START_LOADING')
     commit('MAKE_MUTATION_ID_TASK')
-    const payload = await createTask(state.newTask)
+    const payload = await query(CreateTask, state.newTask)
     commit('PUSH_TASK', payload)
     commit('UPDATE_NEW_TASK', { description: '' })
     commit('FINISH_LOADING')
